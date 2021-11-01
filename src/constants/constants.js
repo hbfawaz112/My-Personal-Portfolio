06174320c5b77ded723aab11ae17d965811262d7
Hussein B. Fawaz <hbfawaz112@gmail.com>
2021-11-01 22:29:57 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,90 +1,101 @@
 export const projects = [
   {
     title: 'Petrol Station System',
-    description: "This project is a about a managment system for any petrol station , consist as a both web version and  mobile application work on both android and ios  . It can manage all the requirment for a petrol station , from container , pumps , customer , Accounting , shipment and many more..  ",
-      image: '/images/Petrol.PNG',
-      tags: ['Flutter','Firebase', 'PHP', 'MySQL', 'Ajax' , 'JQuery'],
+    description: "A managment system for a petrol station , consist of both web app and mobile application .It can manage all the requirment for a petrol station(accounting-shipments-containers...)",
+    image: '/images/Petrol.PNG',
+    tags: ['Flutter', 'Firebase', 'PHP', 'MySQL', 'Ajax', 'JQuery'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/Petrol-Station',
     id: 0,
   },
   {
     title: 'FCSS Web Site',
-    description: "Fawaz Computer & Security System a small company located in Tebnin, this project is a web site for this business contain E-commerce section with add to cart, product detail , check out , a contact page and a store page and more coming out later.., Build using React and designed using Material UI",
-      image: '/images//fcss1.png',
-      tags: ['ReactJS', 'Commerce.js','Material UI'],
+    description: "FCSS small company in Tebnin, this project is a website for this business consist as a E-commerce site with addto cart, product detail , check out , a contact page and a store page.",
+
+    image: '/images/fcss1.png',
+    tags: ['ReactJS', 'Commerce.js', 'Material UI'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/FCSS-Site',
     id: 0,
   },
   {
     title: 'LFA Grassroots',
-    description: "This project is implemented using flutter technologie with mysql backend. Consist as an admin page (web page) to upload all data required to the grassroot football league (annoncment , matches , results , ranking,ect..) , and mobile application that work on both android and ios devices for the users to see these data.",
-      image: '/images/LFA-Grassroots.PNG',
-      tags: ['Flutter', 'PHP', 'MySQL','API'],
+    description: "A system for the lebanese football association (grassroots)contain an admin site to manage (crud) all required data and a mobile app for users to see all these data(results/news/matches/rank).",
+    image: '/images/LFA-Grassroots.PNG',
+    tags: ['Flutter', 'PHP', 'MySQL', 'API'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/LFA-Grassroots',
     id: 0,
   },
   {
+    title: 'Mushrrom Detector',
+    description: " An app used to detect and predict the type of the mushroom by taking a picture of it from a camera or import the image from the gallery. Plus some guide about many kinds of  mushrooms",
+    image: '/images/mushrrom.png',
+    tags: ['Flutter', 'Machine Learning ',' Tenserflow Lite'],
+    source: 'https://google.com',
+    visit: 'https://github.com/hbfawaz112/Mushroom-Detector-Flutter-ML',
+    id: 0,
+  },
+  
+  {
     title: 'UL-FS Form Of Discussion',
-    description:"A website developed using ASP.NET MVC to let students communicate between them by asking public questions or private questions based on many factors , and reply to these questions",
+    description: "A website developed using ASP.NET MVC to let students communicate between them by asking public questions or private questions based on many factors , and reply to these questions",
     image: '/images/ulfs-forum.PNG',
-    tags: ['ASP.NET-MVC ', ' Microsoft SQL Server  ' ,' Bootstrap'],
+    tags: ['ASP.NET-MVC ', ' Microsoft SQL Server  ', ' Bootstrap'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/UL-FS-Form-of-Discussion-ASP.NET-MVC',
     id: 1,
   },
   {
     title: 'Blogging Platform ',
-    description:"A Blog Site containing the following features : Dynamic Blog pages , have a dedicated editor for blogs, you can add/make as many blogs you want, you can add Headings, paragraphs, and Images to the blog post ,have read more blogs section also.",
+    description: "A bloging platform site containing dynamic blog pages,have a dedicated editor for blogs, you can add/make as many blogs you want, you can add Headings,paragraphs,and images to the blog post.",
     image: '/images/blognode.png',
-    tags: ['HTML/CSS','Node.js',' Firebase Firestore'],
+    tags: ['HTML/CSS', 'Node.js', ' Firebase Firestore'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/Blogging-Site-Full-Stack',
     id: 1,
   },
-  
+
   {
     title: 'Moodle ULFS',
-    description:"This is a web project developed using php-mysql , it’s about 2 website one for the instructors to upload files as pdf courses, sessions, … , and one for the students that can acces to their account and download these files.",
+    description: "This is a web project developed using php-mysql , it’s about 2 website one for the instructors to upload files as pdf courses, sessions, … , and one for the students that can acces to their account and download these files.",
     image: '/images/moodle1.png',
-    tags: ['PHP', 'MYSQL' ,'Bootstrap'],
-     source: 'https://google.com',
+    tags: ['PHP', 'MYSQL', 'Bootstrap'],
+    source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/moodle-UL-Faculity-Of-Science',
     id: 1,
   },
   {
-    title: 'Math Game Drag And Drop (JavaFX)',
-    description:"A Desktop game developed developed using JavaFX with the format of FXML and FXML Controller containing 3 section , basic math like count objects , compare numbers  , calculation section{Addition - Substraction - Multiplucation - Division Or Random} , and basic geometrie section, made for kids",
-          image: '/images/SS.png',
-    tags: ['JavaFX'],
+    title: 'Math Game Drag And Drop',
+    description: "A Desktop game containing 3 sections,basic math(count objcets/comparaison),calculation section(Addition-Substraction-Multiplucation-Division-Random) and basic geometrie section, made for kids.",
+    image: '/images/SS.png',
+    tags: ['JavaFX - FXML - Controller'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/Javafx-MathGame-Drag-And-Drop',
     id: 1,
   },
- {
+  {
     title: 'Twitter Clone',
-    description:"A social media website like twitter developed using PHP and MongoDB for the backend and HTML/CSS/BOOTSTRAP for the frontend, Containing authentication to create account and login , post a new tweet , follow and unfollow users , like and comment tweests ,ect.. ",
-        image: '/images/twitter.png',
-    tags: ['PHP','MongoDB' , 'Bootstrap'],
+    description: "A social media site like twitter,containing authentication to create account and login , post a new tweet ,follow and unfollo users , like and comment tweests ,update your profile ....",
+    image: '/images/twitter.png',
+    tags: ['PHP', 'MongoDB', 'Bootstrap'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/Twitter-Clone',
     id: 1,
   },
 
-  
+
   {
     title: 'Football Academies Managment System',
-    description:"A Desktop app developed with java (GUI-swing) and Microsoft SQL Server for the databasr help managing data for  a football academy (Players , Coaches , Training and the academy balance and accounting)",
-       image: '/images/fm.png',
-    tags: ['javaSwing','Microsoft SQL Server'],
+    description: "A Desktop app developed with java (GUI-swing) and Microsoft SQL Server for the databasr help managing data for  a football academy (Players , Coaches , Training and the academy balance and accounting)",
+    image: '/images/fm.png',
+    tags: ['javaSwing', 'Microsoft SQL Server'],
     source: 'https://google.com',
     visit: 'https://github.com/hbfawaz112/Football-Academies-Management-Systeme',
     id: 1,
   },
-  
- 
+
+
 ];
 
 export const TimeLineData = [
