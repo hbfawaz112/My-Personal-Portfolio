ce59d0a57ef6c9f67bac45427f8a34dcc3d59bee
Hussein B. Fawaz <hbfawaz112@gmail.com>
2022-11-18 13:17:40 +0200

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -18,7 +18,7 @@ const Hero = (props) => (
         
         <Button  onClick={(e) => {
       e.preventDefault();
-      window.location.href='https://drive.google.com/file/d/1dSoLFQXjxfNcxAC75tbaFfoYVXGIYdhm/view?usp=sharing';
+      window.location.href='https://drive.google.com/file/d/1_pIrQBNwXZXkJc2rHC3aUcYjMqiMN1aK/view';
       }} >My Resume</Button>
       </LeftSection>
     </Section>
